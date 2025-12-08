414bf434cc022f46901fed39ffc2613683e4aff3
tinhcmus <164772025+Tjkut@users.noreply.github.com>
2025-12-08 14:37:09 +0700

    Update test1_2.c

--- DSA1/TEST1.2/test1_2.c
+++ DSA1/TEST1.2/test1_2.c
@@ -1,1 +1,29 @@
-
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#define MAX 255
+int findChar(char s[],int length,char ch)
+{
+    for(int i=0;i<length;i++)
+    {
+       if(tolower(s[i])==tolower(ch))
+       {
+        return i;
+       }
+    }
+    return -1;
+}
+int main()
+{
+    char s[MAX];
+    printf("nhap chuoi:");
+    fgets(s,MAX,stdin);
+    int len=strlen(s);
+    s[strcspn(s,"\n")]=0;
+    char ch;
+    printf("nhap ky tu can tim:");
+    scanf("%c",&ch);
+  printf("found %c at %d",ch,findChar(s,strlen(s),ch));
+    return 0;
+} 
