a9221952d1ec4e53b7961b355012b80dac21bebc
tinhcmus <164772025+Tjkut@users.noreply.github.com>
2025-12-08 17:10:02 +0700

    Update test1_4.c

--- DSA1/TEST1.4/test1_4.c
+++ DSA1/TEST1.4/test1_4.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX 1000
 
+void insert(char s[], char key, int index)
+{
+    int size_string = strlen(s);
+    for (int i = size_string; i >= index; i--)
+    {
+        s[i + 1] = s[i];
+    }
+    s[index] = key;
+}
+
+int main()
+{
+    char s[MAX];
+    fgets(s, MAX, stdin);
+    s[strcspn(s, "\n")] = 0;
+    char key;
+    scanf(" %c", &key);
+    int index;
+    printf("Nhap vi tri can chen: ");
+    scanf("%d", &index);
+    insert(s, key, index);
+    printf("Chuoi sau khi chen: %s\n", s);
+    return 0;
+}
