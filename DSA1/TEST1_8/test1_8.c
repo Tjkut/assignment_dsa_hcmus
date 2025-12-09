22f8e7692aff47810ad7cc96754d9927ef203662
tinhcmus <164772025+Tjkut@users.noreply.github.com>
2025-12-09 21:53:19 +0700

    Add character deletion functionality in test1_8.c
    Implement function to delete character at specified index

--- DSA1/TEST1_8/test1_8.c
+++ DSA1/TEST1_8/test1_8.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include<stdbool.h>
+#define MAX 200
+void delectChar(char *s,int index)
+{
+     int len = strlen(s);
 
+    if (index < 0 || index >= len) return; // vị trí không hợp lệ
+
+    for (int i = index; i < len; i++)
+    {
+        s[i] = s[i + 1];
+    }
+}
+
+
+
+int main()
+{
+   char s[MAX];
+   int index;
+   printf("nhap chuoi:");
+   fgets(s,MAX,stdin);
+   printf("nhap vi tri can xoa:");
+   scanf("%d",&index);
+   delectChar(s,index);
+   printf("chuoi sau khi xoa:%s",s);
+   printf("%s",s);
+}
