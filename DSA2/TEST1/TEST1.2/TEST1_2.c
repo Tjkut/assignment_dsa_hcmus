940a42e884b986de2832f8325578aba4247a92af
tinhcmus <164772025+Tjkut@users.noreply.github.com>
2025-12-15 16:58:52 +0700

    Update TEST1_2.c

--- DSA2/TEST1/TEST1.2/TEST1_2.c
+++ DSA2/TEST1/TEST1.2/TEST1_2.c
@@ -1,1 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define MAX 100
 
+int orderedLinearSearch(float arr[], int size, float value)
+{
+    int left = 0;
+    int right = size - 1;
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        if (arr[mid] == value)
+        {
+            return mid;
+        }
+        else if (arr[mid] < value)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    float arr[MAX];
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%f", &arr[i]);
+    }
+    float value;
+    scanf("%f", &value);
+    int index = orderedLinearSearch(arr, n, value);
+    if (index != -1)
+    {
+        printf(" %d\n", index);
+    }
+    else
+    {
+        printf("Not found\n");
+        return 0;
+    }
+}
