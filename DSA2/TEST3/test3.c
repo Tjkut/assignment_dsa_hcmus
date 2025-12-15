89eec1c4af49ec8c5611eace793f8a615329cfdf
tinhcmus <164772025+Tjkut@users.noreply.github.com>
2025-12-15 18:08:49 +0700

    Update test3.c

--- DSA2/TEST3/test3.c
+++ DSA2/TEST3/test3.c
@@ -1,1 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
 
+
+int main()
+{
+    char s[]="bcdefghijklmnopqrstuvwxyz";
+    int mark[26]={0};
+    for(int i=0;i<strlen(s);i++){
+        mark[s[i]-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        if(mark[i]==0){
+            printf("%c at %d is missing",i+'a',i);
+            break;
+        }
+    }
+//dung mang danh dau de luu cac ky tu da xuat hien
+// sau do duyet qua mang danh dau de tim ky tu dau tien khong xuat hien
+    return 0;
+  
+}
